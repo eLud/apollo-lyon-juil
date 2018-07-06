a1c1a37bfd711693fea8ed4c3fff0cb285427076
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2018-07-06 10:34:54 +0200

    Demo animation

--- Projects/Fitness/Fitness/ViewController.swift
+++ Projects/Fitness/Fitness/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var redView: UIView!
+
+    var button: UIButton?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        button = UIButton(type: .infoLight)
+        button?.translatesAutoresizingMaskIntoConstraints = false
+
+        let centerConstraint = button?.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        view.addSubview(button!)
+
+        let verticalPosition: NSLayoutConstraint?
+
+        if #available(iOS 11.0, *) {
+            verticalPosition = button?.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        } else {
+            verticalPosition = button?.topAnchor.constraint(equalTo: topLayoutGuide.topAnchor, constant: 8)
+        }
+
+        verticalPosition?.isActive = true
+        centerConstraint?.isActive = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +42,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addNewView(_ sender: Any) {
+
+        let newView = UIView()
+        newView.backgroundColor = UIColor.blue
+
+        UIView.animate(withDuration: 0.4, animations: {
+//            self.stackView.addArrangedSubview(newView)
+//            self.stackView.insertArrangedSubview(self.redView, at: 0)
+            self.stackView.arrangedSubviews[2].isHidden = !self.stackView.arrangedSubviews[2].isHidden
+
+            self.stackView.arrangedSubviews[1].backgroundColor = .white
+
+            self.stackView.layoutIfNeeded()
+
+        }, completion: nil)
+    }
 
 }
 
